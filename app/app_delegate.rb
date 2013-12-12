36d31b7ad71c0ea0515591b3fe2ee264e9d8a206
Duncan Robertson <duncan@whomwah.com>
2013-12-12 16:57:44 +0000

    don’t need instance variables here

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -9,14 +9,14 @@ class AppDelegate
   end
 
   def build_status
-    @status_bar_image     = NSImage.imageNamed("k_logo_col_18x18")
-    @status_bar_image_alt = NSImage.imageNamed("k_logo_bw_18x18")
+    status_bar_image     = NSImage.imageNamed("k_logo_col_18x18")
+    status_bar_image_alt = NSImage.imageNamed("k_logo_bw_18x18")
 
     status_bar = NSStatusBar.systemStatusBar
     bar = status_bar.statusItemWithLength(NSVariableStatusItemLength)
     bar.retain
-    bar.setImage(@status_bar_image)
-    bar.setAlternateImage(@status_bar_image_alt)
+    bar.setImage(status_bar_image)
+    bar.setAlternateImage(status_bar_image_alt)
     bar.setHighlightMode(true)
     @menu = setupMenu
     bar.setMenu(@menu)
