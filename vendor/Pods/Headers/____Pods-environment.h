519dc2224f07419bd035f41f74e91cc98ea7b2f3
Duncan Robertson <duncan@whomwah.com>
2014-01-08 20:17:27 +0000

    add MASSShortcut cocoapod

--- vendor/Pods/Headers/____Pods-environment.h
+++ vendor/Pods/Headers/____Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// MASShortcut
+#define COCOAPODS_POD_AVAILABLE_MASShortcut
+#define COCOAPODS_VERSION_MAJOR_MASShortcut 1
+#define COCOAPODS_VERSION_MINOR_MASShortcut 2
+#define COCOAPODS_VERSION_PATCH_MASShortcut 3
+
 // SocketRocket
 #define COCOAPODS_POD_AVAILABLE_SocketRocket
 #define COCOAPODS_VERSION_MAJOR_SocketRocket 0
