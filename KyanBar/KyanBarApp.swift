07328d5a61704bc0188f8f9930eeeabe7c8837a4
Duncan Robertson <duncan@kyan.com>
2021-12-02 11:08:23 +0000

    Adding some inline comments

--- KyanBar/KyanBarApp.swift
+++ KyanBar/KyanBarApp.swift
@@ -13,7 +13,8 @@ import Sparkle
 struct kyanApp: App {
   // swiftlint:disable:next weak_delegate
   @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-  
+
+  // This is our Scene. We are not using Settings
   var body: some Scene {
     Settings {
       EmptyView()
@@ -21,32 +22,40 @@ struct kyanApp: App {
   }
 }
 
+// Our AppDelegae will handle our menu
 class AppDelegate: NSObject, NSApplicationDelegate {
   static private(set) var instance: AppDelegate!
-  
+
+  // The NSStatusBar manages a collection of status items displayed within a system-wide menu bar.
   lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+  // Define what updaterController will be
   let updaterController: SPUStandardUpdaterController
+  // Create an instance of our custom main menu we are building
   let menu = MainMenu()
-  
+
   override init() {
+    // This plumbs in the 3rd party Sparkle updater framework
     // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater() later
     // This is where you can also pass an updater delegate if you need one
     updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
   }
-  
+
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     AppDelegate.instance = self
-    
+
     // TODO: This should not be probably created here
+    // This is the menu items that users can click to check for updates
     let checkForUpdatesMenuItem = NSMenuItem(
       title: "Check For Updates...",
       action: #selector(SPUStandardUpdaterController.checkForUpdates(_:)),
       keyEquivalent: ""
     )
     checkForUpdatesMenuItem.target = updaterController
-    
+
+    // Here we are using a custom icon found in Assets.xcassets
     statusBarItem.button?.image = NSImage(named: NSImage.Name("kyan-logo"))
     statusBarItem.button?.imagePosition = .imageLeading
+    // Assign our custom menu to the status bar
     statusBarItem.menu = menu.build(updatesMenuItem: checkForUpdatesMenuItem)
   }
 }
--- KyanBar/MainMenu.swift
+++ KyanBar/MainMenu.swift
@@ -8,8 +8,13 @@
 import Cocoa
 import SwiftUI
 
+// This is our custom menu that will appear when users
+// click on the menu bar icon
 class MainMenu: NSObject {
+  // A new menu instance ready to add items to
   let menu = NSMenu()
+  // These are the available link which will appear as part of
+  // the menu. These could be remotely loaded or stored in environment vars
   let menuItems: [String: String] = [
     "Handbook": "https://handbook.kyan.com/",
     "Holiday": "https://app.timetastic.co.uk/wallchart",
@@ -17,28 +22,44 @@ class MainMenu: NSObject {
     "Jukebox": "http://jukebox.kyan.com"
   ]
 
+  // function called by KyanBarApp to create the menu
   func build(updatesMenuItem: NSMenuItem) -> NSMenu {
+    // Initialse the custom now playing view
     let nowPlayingView = NowPlayingView()
+    // We need this to allow use to stick a SwiftUI view into a
+    // a location an NSView would normally be placed
     let contentView = NSHostingController(rootView: nowPlayingView)
+    // Setting a size for our now playing view
     contentView.view.frame.size = CGSize(width: 200, height: 80)
-    
+
+    // This is where we actually add our now playing view to the menu
     let customMenuItem = NSMenuItem()
     customMenuItem.view = contentView.view
     menu.addItem(customMenuItem)
-    
+
+    // Adding a seperator
     menu.addItem(NSMenuItem.separator())
-    
+
+    // We add an About pane.
     let aboutMenuItem = NSMenuItem(
       title: "About KyanBar",
       action: #selector(about),
       keyEquivalent: ""
     )
+    // This is important so that our #selector
+    // targets the `about` func in this file
     aboutMenuItem.target = self
-    
+
+    // This is where we actually add our about item to the menu
     menu.addItem(aboutMenuItem)
+    // This is where we actually add the updates menu item we pass in
     menu.addItem(updatesMenuItem)
+
+    // Adding a seperator
     menu.addItem(NSMenuItem.separator())
-    
+
+    // Loop though all our links and create a new menu item for
+    // each, and then add it to the menu
     for (title, link) in menuItems {
       let menuItem = NSMenuItem(
         title: title,
@@ -50,9 +71,11 @@ class MainMenu: NSObject {
 
       menu.addItem(menuItem)
     }
-    
+
+    // Adding a seperator
     menu.addItem(NSMenuItem.separator())
-    
+
+    // Adding a quit menu item
     let quitMenuItem = NSMenuItem(
       title: "Quit KyanBar",
       action: #selector(quit),
@@ -64,16 +87,23 @@ class MainMenu: NSObject {
     return menu
   }
 
+  // The selector that takes a link and opens it
+  // in your default browser
   @objc func linkSelector(sender: NSMenuItem) {
     let link = sender.representedObject as! String
     guard let url = URL(string: link) else { return }
     NSWorkspace.shared.open(url)
   }
-  
+
+  // The selector that opens a standard about pane.
+  // You can see we also customise what appears in our
+  // about pane by creating a Credits.html file in the root
+  // of the project
   @objc func about(sender: NSMenuItem) {
     NSApp.orderFrontStandardAboutPanel()
   }
-  
+
+  // The selector that quits the app
   @objc func quit(sender: NSMenuItem) {
     NSApp.terminate(self)
   }
